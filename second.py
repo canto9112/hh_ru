54f2460794199f7b64287fa4efbc021c2e8b3212
canto9112 <cantoweb112@gmail.com>
2021-02-01 22:27:54 +0300

    Вывел среднюю ЗП со всех страниц HH.ru

--- second.py
+++ second.py
@@ -35,27 +35,28 @@ def get_salary(url, vacancy, area, period, page, per_page):
 def predict_rub_salary(salarys):
     average = []
     for salary in salarys:
-        if salary is None:
-            None
-        elif salary['currency'] != 'RUR':
-            None
-        elif salary['currency'] == 'RUR':
-            from_salary = salary['from']
-            to_salary = salary['to']
-            if from_salary is None and to_salary is not None:
-                average_salary = to_salary * 0.8
-                average.append(int(average_salary))
-            elif from_salary is not None and to_salary is None:
-                average_salary = from_salary * 1.2
-                average.append(int(average_salary))
-            else:
-                average_salary = (from_salary + to_salary) / 2
-                average.append(int(average_salary))
-    vacancies_processed = len(average)
-    sum_salary = 0
-    for selary in average:
-        sum_salary += selary
-    average_salarys = sum_salary / vacancies_processed
+        for sal in salary:
+            if sal is None:
+                None
+            elif sal['currency'] != 'RUR':
+                None
+            elif sal['currency'] == 'RUR':
+                from_salary = sal['from']
+                to_salary = sal['to']
+                if from_salary is None and to_salary is not None:
+                    average_salary = to_salary * 0.8
+                    average.append(int(average_salary))
+                elif from_salary is not None and to_salary is None:
+                    average_salary = from_salary * 1.2
+                    average.append(int(average_salary))
+                else:
+                    average_salary = (from_salary + to_salary) / 2
+                    average.append(int(average_salary))
+        vacancies_processed = len(average)
+        sum_salary = 0
+        for selary in average:
+            sum_salary += selary
+        average_salarys = sum_salary / vacancies_processed
     return vacancies_processed, int(average_salarys)
 
 
@@ -79,13 +80,13 @@ def get_numbers_first_page(url, vacancy, per_page, area, period):
 def get_dict_vacancys(vacancies, url, area, period, page, per_page):
     dict_vacations = {}
     for vacancy in vacancies:
-        salarys = get_salary(url, vacancy, area, period, page, per_page)
+        salarys = get_all_salarys_list(api_url, vacancy, per_page, area, period)
         vacancies_processed, average_salary = predict_rub_salary(salarys)
         vacancies_found = get_vacancies_found(url, vacancy, area, period)
         dict_vacations.update({vacancy:
-                     {'vacancies_found': vacancies_found,
-                      'vacancies_processed': vacancies_processed,
-                      'average_salary': average_salary}})
+                     {'Найдено вакансий': vacancies_found,
+                      'Обработано вакансий': vacancies_processed,
+                      'Средняя ЗП': average_salary}})
     pprint(dict_vacations)
     return dict_vacations
 
@@ -99,8 +100,8 @@ def get_all_salarys_list(url, vacancy, per_page, area, period):
             salarys.append(salary)
         else:
             pass
-    pprint(salarys)
     return salarys
+
 
 if __name__ == '__main__':
     vacancies = ['JavaScript', 'Java', 'Python']
@@ -110,41 +111,8 @@ if __name__ == '__main__':
     page = 0
     per_page = 100
 
-    # get_dict_vacancys(vacancies, api_url, area, period, page, per_page)
-    get_all_salarys_list(api_url, 'Python', per_page, area, period)
+    get_dict_vacancys(vacancies, api_url, area, period, page, per_page)
 
-
-
-
-
-# salarys = get_all_salarys_list(api_url, 'Python', per_page, area, period)
-# average = []
-# for salary in salarys:
-#     for sal in salary:
-#         if sal is None:
-#             None
-#         elif sal['currency'] != 'RUR':
-#             None
-#         elif sal['currency'] == 'RUR':
-#             from_salary = sal['from']
-#             to_salary = sal['to']
-#             if from_salary is None and to_salary is not None:
-#                 average_salary = to_salary * 0.8
-#                 average.append(int(average_salary))
-#             elif from_salary is not None and to_salary is None:
-#                 average_salary = from_salary * 1.2
-#                 average.append(int(average_salary))
-#             else:
-#                 average_salary = (from_salary + to_salary) / 2
-#                 average.append(int(average_salary))
-#     vacancies_processed = len(average)
-#     sum_salary = 0
-#     for selary in average:
-#         sum_salary += selary
-#     average_salarys = sum_salary / vacancies_processed
-#
-# print('Обработано объявлений -', vacancies_processed)
-# print('Программист Python', int(average_salarys))
 
 
 
