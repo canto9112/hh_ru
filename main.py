45d349daa3a709a7d755be0ac8aebc6a58fbd9b7
canto9112 <cantoweb112@gmail.com>
2021-02-04 18:46:10 +0300

    Получение зарплат с HH.ru завершил

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 
+
 def get_first_page_vacancy(vacancy, page):
     params = {
         'text': f'Программист {vacancy}',
@@ -34,17 +35,13 @@ def predict_rub_salary(vacancy):
 
 
 if __name__ == '__main__':
-    # name_vacancies = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP',
-    #                   'C++', 'Swift', 'C#', 'C', 'Go']
-    languages = ['C++']
+
+    languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'Swift', 'C#', 'C', 'Go']
     number_page = 0
 
-
-    dict_vacations = {}
+    average_salary_languages = {}
     for language in languages:
-
         first_page_response = get_first_page_vacancy(language, number_page)
-
         vacancies_found = first_page_response['found']
         pages = first_page_response['pages']
         firs_page_vacancies = first_page_response['items']
@@ -55,7 +52,6 @@ if __name__ == '__main__':
             if page == 19:
                 break
 
-
         all_salary = []
         sum_salarys = 0
         for vacancy in vacancies:
@@ -65,22 +61,16 @@ if __name__ == '__main__':
             else:
                 all_salary.append(salary)
                 sum_salarys += salary
-        # print(sum_salarys)
+
         vacancies_processed = len(all_salary)
-        print('Обработано вакансий', vacancies_processed)
         average_salary = sum_salarys / vacancies_processed
-        print('Средняя Зп', int(average_salary))
 
+        average_salary_languages.update({language:
+                                        {'Найдено вакансий': vacancies_found,
+                                         'Обработано вакансий': vacancies_processed,
+                                         'Средняя ЗП': int(average_salary)}})
 
-    #         salary.append(sal['salary'])
-    #
-    #     vacancies_processed, average_salarys = predict_rub_salary(salary)
-    #     dict_vacations.update({vacancy:
-    #                                {'Найдено вакансий': vacancies_found,
-    #                                 'Обработано вакансий': vacancies_processed,
-    #                                 'Средняя ЗП': average_salarys}})
-    #
-    # pprint(dict_vacations)
+    pprint(average_salary_languages)
 
 
 
