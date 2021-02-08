65820bcb43d09740a110f323b86695123edac41e
canto9112 <cantoweb112@gmail.com>
2021-02-08 19:04:43 +0300

    Оформил файл super_job.py

---
+++ super_job.py
@@ -0,0 +1,59 @@
+import requests
+
+
+def get_first_page_vacancy(vacancy, api_key):
+    url = '	https://api.superjob.ru/2.0/vacancies/'
+    headers = {'X-Api-App-Id': api_key}
+    params = {
+        'count': 100,
+        'keyword': f'Программист {vacancy}',
+        'town': 4,
+        'catalogues': 33
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def predict_rub_salary_for_superJob(vacancy):
+    payment_from = vacancy['payment_from']
+    payment_to = vacancy['payment_to']
+
+    if payment_from == 0 and payment_to == 0:
+        return None
+    elif payment_from == 0 and payment_to != 0:
+        average_salary = payment_to * 0.8
+    elif payment_from != 0 and payment_to == 0:
+        average_salary = payment_from * 1.2
+    else:
+        average_salary = (payment_from + payment_to) / 2
+    return average_salary
+
+
+def get_vacancies_dict(languages, api_key):
+    average_salary_languages = {}
+
+    for language in languages:
+        first_page_vacancy = get_first_page_vacancy(language, api_key)
+        total = first_page_vacancy['total']
+
+        all_salary = []
+        sum_salarys = 0
+        for number_vacancy in range(0, total):
+            if number_vacancy < 100:
+                vacancy = first_page_vacancy['objects'][number_vacancy]
+                salary = predict_rub_salary_for_superJob(vacancy)
+                if salary is None:
+                    pass
+                else:
+                    all_salary.append(salary)
+                    sum_salarys += salary
+
+        vacancies_processed = len(all_salary)
+        average_salary = sum_salarys / vacancies_processed
+
+        average_salary_languages.update({language:
+                                        {'vacancies_found': total,
+                                         'vacancies_processed': vacancies_processed,
+                                         'average_salary': int(average_salary)}})
+    return average_salary_languages
