b89b47da7e816ea570be5b1ae33d73e86b7997b3
canto9112 <cantoweb112@gmail.com>
2021-02-08 19:24:30 +0300

    Офрмил main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import super_job
 import hh_ru
 import utils
+import os
+from dotenv import load_dotenv
 
+if __name__ == "__main__":
+    load_dotenv()
 
-if __name__ =="__main__":
+    super_job_api_key = os.getenv('SUPER_JOB_KEY')
+
     languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'Swift', 'C#', 'C', 'Go']
-    hh_title_name = 'hh.ru'
-    super_job_title_name = 'superJob'
 
-    super_job = super_job.get_vacancies_dict(languages)
-    hh_ru = hh_ru.get_vacancies_dict(languages)
+    hh_table_name = 'hh.ru'
+    super_job_table_name = 'superJob'
 
-    utils.get_terminaltables(hh_ru, hh_title_name)
+    super_job_dict = super_job.get_vacancies_dict(languages, super_job_api_key)
+    hh_ru_dict = hh_ru.get_vacancies_dict(languages)
+
+    hh_ru_table = utils.get_terminaltables(hh_ru_dict, hh_table_name)
+    print(hh_ru_table)
     print('========')
-    utils.get_terminaltables(super_job, super_job_title_name)
+    super_job_table = utils.get_terminaltables(super_job_dict, super_job_table_name)
+    print(super_job_table)
