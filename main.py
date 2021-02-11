54054b5907350b10fbe31548d2a4cab87b3c8b77
canto9112 <cantoweb112@gmail.com>
2021-02-11 14:02:48 +0300

    Затупил

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ if __name__ == "__main__":
 
     super_job_api_key = os.getenv('SUPER_JOB_KEY')
 
-    languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'Swift', 'C#', 'C', 'Go']
+    # languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'Swift', 'C#', 'C', 'Go']
+    languages = ['JavaScript']
 
     hh_table_name = 'hh.ru'
     super_job_table_name = 'superJob'
 
     super_job_vacancies = super_job.get_average_salary_languages_superJob(languages, super_job_api_key)
-    super_job_table = utils.get_terminal_table(super_job_vacancies, super_job_table_name)
-    print(super_job_table)
-
-    print('========')
-
-    hh_ru_vacancies = hh_ru.get_average_salary_languages_hh(languages)
-    hh_ru_table = utils.get_terminal_table(hh_ru_vacancies, hh_table_name)
-    print(hh_ru_table)
+    # super_job_table = utils.get_terminal_table(super_job_vacancies, super_job_table_name)
+    # print(super_job_table)
+    #
+    # print('========')
+    #
+    # hh_ru_vacancies = hh_ru.get_average_salary_languages_hh(languages)
+    # hh_ru_table = utils.get_terminal_table(hh_ru_vacancies, hh_table_name)
+    # print(hh_ru_table)
 
 
 
--- super_job.py
+++ super_job.py
@@ -2,7 +2,7 @@ import requests
 from pprint import pprint
 
 
-def get_first_page_vacancies(vacancy, api_key, page):
+def get_vacancies(vacancy, api_key, page):
     url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {'X-Api-App-Id': api_key}
     params = {
@@ -17,40 +17,40 @@ def get_first_page_vacancies(vacancy, api_key, page):
     return response.json()
 
 
-def predict_rub_salary_for_superJob(vacancy):
-    payment_from = vacancy['payment_from']
-    payment_to = vacancy['payment_to']
+def predict_rub_salary_for_superJob(vacancy, total):
+    for number_vacancy in range(0, total):
 
-    if payment_from == 0 and payment_to == 0:
-        return None
-    elif payment_from == 0 and payment_to != 0:
-        average_salary = payment_to * 0.8
-    elif payment_from != 0 and payment_to == 0:
-        average_salary = payment_from * 1.2
-    else:
-        average_salary = (payment_from + payment_to) / 2
-    return average_salary
+        payment_from = vacancy['objects'][number_vacancy]['payment_from']
+        payment_to = vacancy['objects'][number_vacancy]['payment_to']
+
+        if payment_from == 0 and payment_to == 0:
+            return None
+        elif payment_from == 0 and payment_to != 0:
+            average_salary = payment_to * 0.8
+        elif payment_from != 0 and payment_to == 0:
+            average_salary = payment_from * 1.2
+        else:
+            average_salary = (payment_from + payment_to) / 2
+        return average_salary
 
 
 def get_average_salary_languages_superJob(languages, api_key):
     average_salary_languages = {}
 
     for language in languages:
-        first_page_vacancy = get_first_page_vacancies(language, api_key)
-        total = first_page_vacancy['total']
+        all_vacancies = get_all_vacancies_superJob(language, api_key)
+        total = all_vacancies[0]['total']
 
         all_salary = []
         sum_salarys = 0
-        for number_vacancy in range(0, total):
-            if number_vacancy < 100:
-                vacancy = first_page_vacancy['objects'][number_vacancy]
-                salary = predict_rub_salary_for_superJob(vacancy)
-                if salary is None:
-                    pass
-                else:
-                    all_salary.append(salary)
-                    sum_salarys += salary
-
+        for vacancy in all_vacancies:
+            salary = predict_rub_salary_for_superJob(vacancy, total)
+            if salary is None:
+                pass
+            else:
+                all_salary.append(salary)
+                sum_salarys += salary
+        print(all_salary)
         vacancies_processed = len(all_salary)
         average_salary = sum_salarys / vacancies_processed
 
@@ -60,3 +60,15 @@ def get_average_salary_languages_superJob(languages, api_key):
                                          'average_salary': int(average_salary)}})
     print(average_salary_languages)
     return average_salary_languages
+
+
+def get_all_vacancies_superJob(language, api_key):
+    all_vacancies = []
+    more = True
+    page = 0
+    while more:
+        first_page_vacancy = get_vacancies(language, api_key, page)
+        all_vacancies.append(first_page_vacancy)
+        more = first_page_vacancy['more']
+        page += 1
+    return all_vacancies
