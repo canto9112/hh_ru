bc841d140ddfe83b7ea4ac256c0691badae34d0d
canto9112 <cantoweb112@gmail.com>
2021-02-11 19:06:47 +0300

    Высушил код

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ if __name__ == "__main__":
     super_job_api_key = os.getenv('SUPER_JOB_KEY')
 
     languages = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'Swift', 'C#', 'C', 'Go']
-    # languages = ['Java', 'Python', 'Ruby']
 
     hh_table_name = 'hh.ru'
     super_job_table_name = 'superJob'
 
-    # super_job_vacancies = super_job.get_average_salary_languages_superJob(languages, super_job_api_key)
-    # super_job_table = utils.get_terminal_table(super_job_vacancies, super_job_table_name)
-    # print(super_job_table)
-    #
-    # print('========')
+    super_job_vacancies = super_job.get_average_salary_languages_superJob(languages, super_job_api_key)
+    super_job_table = utils.get_terminal_table(super_job_vacancies, super_job_table_name)
+    print(super_job_table)
+
+    print('========')
 
     hh_ru_vacancies = hh_ru.get_average_salary_languages_hh(languages)
     hh_ru_table = utils.get_terminal_table(hh_ru_vacancies, hh_table_name)
