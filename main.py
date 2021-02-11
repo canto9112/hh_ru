c456f42538f538dc74bf9a141984c6234d594532
canto9112 <cantoweb112@gmail.com>
2021-02-11 10:05:15 +0300

    Подровнял структуру

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ if __name__ == "__main__":
     super_job_table_name = 'superJob'
 
     super_job_vacancies = super_job.get_average_salary_languages_superJob(languages, super_job_api_key)
-    hh_ru_vacancies = hh_ru.get_average_salary_languages_hh(languages)
-
-    hh_ru_table = utils.get_terminal_table(hh_ru_vacancies, hh_table_name)
-    print(hh_ru_table)
-    print('========')
     super_job_table = utils.get_terminal_table(super_job_vacancies, super_job_table_name)
     print(super_job_table)
+
+    print('========')
+
+    hh_ru_vacancies = hh_ru.get_average_salary_languages_hh(languages)
+    hh_ru_table = utils.get_terminal_table(hh_ru_vacancies, hh_table_name)
+    print(hh_ru_table)
+
+
 
